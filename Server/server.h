42970beb12e5e9fdfe1dfbd5aab479a8008ff760
Peter Essam <75110347+peteressam23@users.noreply.github.com>
2023-09-30 16:06:30 +0300

    server.c modify

--- Server/server.h
+++ Server/server.h
@@ -35,29 +35,7 @@ typedef struct ST_accountsDB_t
      uint8_t primaryAccountNumber[20];
 }ST_accountsDB_t;
 
-//Create a global array of ST_accountsDB_t for the valid accounts database.
-ST_accountsDB_t accountsDB[255];
 
-//Create a global array of ST_transaction_t for the valid transactions database.
-ST_transaction_t transactionDB_t[255];
-
-//Fill in the array of transactionDB_t initially with Zeros
-transactionDB_t[255] = {0};
-
-
-
-//Fill in the array initially with any valid data.
-accountsDB[255] = {
-
-     {2000.0  , RUNNING , "1020304050607080"} ,
-     {5000.0  , BLOCKED , "2030405060708010"} , 
-     {10000.0 , BLOCKED , "3040506070801020"} , 
-     {15500.0 , RUNNING , "4050607080102030"} ,
-     {100000.0, RUNNING , "5060708010203040"} , 
-     {8900.0  , BLOCKED , "6070801020304050"} , 
-     {6850.0  , RUNNING , "7080102030405060"} , 
-     {1000.0  , RUNNING , "8010203040506070"}
-};
 
 
 
