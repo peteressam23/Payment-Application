5d0feb633b14996fe94f9f1b73b33fdcdf568b8e
mari0saad <109170992+mari0saad@users.noreply.github.com>
2023-10-01 14:47:43 +0300

    update isBelowMaxAmountTest

--- Terminal/terminal.c
+++ Terminal/terminal.c
@@ -376,6 +376,9 @@ void isBelowMaxAmountTest(void)
         case 0:
             strcpy_s(result, 30, "TERMINAL_OK");
             break;
+        case 4:
+            strcpy_s(result, 30, "INVALID_AMOUNT");
+            break;
         case 5:
             strcpy_s(result, 30, "EXCEED_MAX_AMOUNT");
             break;
@@ -386,12 +389,8 @@ void isBelowMaxAmountTest(void)
 
         }
         printf("\n\nTester Name :%sFunction Name: isBelowMaxAmount \nTest case %d:\nInput Data:%s Expected result:%sActual result: %s\n-----------------\n"
-            , testerName, iterate + 1, inputFromUser, expectedCase, result);
-            continue;	
+            , testerName, iterate + 1, inputFromUser, expectedCase, result);	
 	}
-    strcpy_s(result, 30, "invalid_amount");
-    printf("\n\nTester Name :%sFunction Name: isBelowMaxAmount \nTest case %d:\nInput Data:%s Expected result:%sActual result: %s\n-----------------\n"
-            , testerName, iterate + 1, inputFromUser, expectedCase, result);
 }
 
 /********************************************************************************************************************************/
